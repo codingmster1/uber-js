2e6c58f355c2239fd83020be9af654d08dc890d6
Jeffrey Doyle <doylejeff98@gmail.com>
2024-09-14 18:48:21 -0400

    assets moved

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,11 +11,17 @@ import 'react-native-reanimated';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  /* const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  }); */
+  const [loaded] = useFonts({
+   "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  }); 
 
-/*   useEffect(() => {
+   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
     }
@@ -23,7 +29,7 @@ export default function RootLayout() {
 
   if (!loaded) {
     return null;
-  } */
+  }
 
   return (
       <Stack>
