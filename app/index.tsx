157808f4ccb752d7dae0e122db52a43fa8b13929
Jeffrey Doyle <doylejeff98@gmail.com>
2024-09-15 15:04:21 -0400

    routes and directories

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,10 @@
- import React from 'react'
+ import { Redirect } from 'expo-router';
+import React from 'react'
  import { Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
  
  const Home = () => {
-   return (
-    <SafeAreaView>
-        <Text>Home</Text>
-    </SafeAreaView>
-   )
+   return <Redirect href="/(auth)/welcome" />
  }
  
  export default Home;
