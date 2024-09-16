021887a1452ba2113cbd8f5d6996d4fe68e5108f
Jeffrey Doyle <doylejeff98@gmail.com>
2024-09-16 13:06:43 -0400

    constants

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,9 +1,17 @@
 import { router } from 'expo-router';
-import { Text, TouchableOpacity  } from 'react-native'
+import { useRef, useState } from 'react';
+import { Text, TouchableOpacity, View  } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Swiper from 'react-native-swiper';
+
+import { onboarding } from "@/constants";
 
 const Onboarding = () => {
-  return (
+    const [activeIndex, setActiveIndex] = useState(0);
+    const swiperRef = useRef<Swiper>(null);
+    const isLastSlide = activeIndex === onboarding.length - 1;
+
+    return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
         onPress={() => {
@@ -13,6 +21,24 @@ const Onboarding = () => {
       >
         <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+            <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+          }
+          activeDot={
+            <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+          }
+          onIndexChanged={(index) => setActiveIndex(index)}
+        >
+            {onboarding.map((item) => (
+          <View >
+           <Text>{item.title}</Text>
+           </View>
+            ))}
+      </Swiper>
 
    </SafeAreaView>
   )
