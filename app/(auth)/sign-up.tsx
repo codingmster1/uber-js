ce894cd46fa8e986cd540527ed979f73c8a449c5
Jeffrey Doyle <doylejeff98@gmail.com>
2024-09-21 13:24:31 -0400

    app.json configured and mock drivers array

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -54,7 +54,15 @@ const SignUp = () => {
           code: verification.code,
         });
         if (completeSignUp.status === "complete") {
-        
+            await fetchAPI("/(api)/user", 
+                {
+                    method: "POST",
+                    body: JSON.stringify({
+                      name: form.name,
+                      email: form.email,
+                      clerkId: completeSignUp.createdUserId,
+                    }),
+                })
           await setActive({ session: completeSignUp.createdSessionId });
           setVerification({
             ...verification,
