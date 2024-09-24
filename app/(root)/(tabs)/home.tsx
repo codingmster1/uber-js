9ab126ac8faa525d15f114607ce1408ac90c8a7c
Jeffrey Doyle <doylejeff98@gmail.com>
2024-09-24 13:43:41 -0400

    fixed googleinput props error rendered search

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -6,6 +6,11 @@ import { FlatList } from 'react-native-gesture-handler'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { icons, images } from '@/constants'
+import GoogleTextInput from '@/app/components/GoogleTextInput';
+
+
+
+
 const recentRides = 
 [
     {
@@ -111,9 +116,9 @@ export default function Page() {
   const { user } = useUser()
   const loading = false;
 
-const handleSignOut = () => {
+const handleSignOut = () => {}
+const handleDestinationPress = () => {}
 
-}
 
   return (
     <GestureHandlerRootView>
@@ -158,6 +163,12 @@ ListHeaderComponent={(
     <Image source={icons.out} className="w-4 h-4" />
     </TouchableOpacity>
     </View>
+
+    <GoogleTextInput
+    icon={icons.search}
+    containerStyle="bg-white shadow-md shadow-neutral-300"
+    handlePress={handleDestinationPress}
+    />
     </>
 )}
 
--- app/components/GoogleTextInput.tsx
+++ app/components/GoogleTextInput.tsx
@@ -82,3 +82,21 @@ const GoogleTextInput = ({
 };
 
 export default GoogleTextInput; */
+import { icons } from "@/constants";
+import { GoogleInputProps } from "@/types/type";
+import { TextInputProps } from "react-native";
+import { Text, View } from "react-native";
+
+const GoogleTextInput = ({
+    icon,
+    initialLocation,
+    containerStyle,
+    textInputBackgroundColor,
+    handlePress,
+} : GoogleInputProps) => (
+<View className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle}`}>
+    <Text>Search</Text>
+</View>
+)
+
+export default GoogleTextInput;
