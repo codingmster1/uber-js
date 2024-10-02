c3884e5bd1e2da693aed5b2c41b0a57a53b0681e
Jeffrey Doyle <doylejeff98@gmail.com>
2024-10-02 13:27:26 -0400

    map rendered on location screen

--- app/components/RideLayout.tsx
+++ app/components/RideLayout.tsx
@@ -2,8 +2,9 @@ import { icons } from "@/constants";
 import { router } from "expo-router";
 import { Image, Text, TouchableOpacity, TouchableOpacityBase, View } from "react-native"
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Map from "./Map";
 
-const RideLayout = ({children }: { children: React.ReactNode}) => {
+const RideLayout = ({ title, children }: { title: string;  children: React.ReactNode}) => {
 return (
  <GestureHandlerRootView>
 <View className="flex-1 bg-white">
@@ -11,12 +12,17 @@ return (
     <View className="flex flex-row absolute z-10 top-16 items-center
     justify-start px-5">
 <TouchableOpacity onPress={() => router.back()}>
-  <View>
+  <View className="w-10 h-10 bg-white rounded-full
+  items-center justify-center">
     <Image source={icons.backArrow} resizeMode="contain"
     className="w-6 h-6" />
   </View>
   </TouchableOpacity>
+  <Text className="text-xl font-JakartaSemiBold ml-5">
+    {title || "Go Back"}
+    </Text>
     </View>
+    <Map />
   </View>
   </View> 
  </GestureHandlerRootView>
