3f4c232d4e566bd617b13ea454f7896f7a554913
Jeffrey Doyle <doylejeff98@gmail.com>
2024-10-02 13:50:43 -0400

    custom button find route and confirm route created

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,6 +1,10 @@
 import { Text, View } from "react-native";
 import { useLocationStore } from "../store";
 import RideLayout from "../components/RideLayout";
+import GoogleTextInput from "../components/GoogleTextInput";
+import { icons } from "@/constants";
+import CustomButton from "../components/CustomButton";
+import { router } from "expo-router";
 
 
  const FindRide = () => {
@@ -11,8 +15,34 @@ import RideLayout from "../components/RideLayout";
         setUserLocation,
       } = useLocationStore();
   return (
-    <RideLayout>
-        <Text className="text-2xl">Find Ride</Text>
+    <RideLayout title="Ride">
+ <View className="my-3">
+        <Text className="text-lg font-JakartaSemiBold mb-3">From</Text>
+
+        <GoogleTextInput
+          icon={icons.target}
+          initialLocation={userAddress!}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="#f5f5f5"
+          handlePress={(location) => setUserLocation(location)}
+        />
+      </View>
+      <View className="my-3">
+        <Text className="text-lg font-JakartaSemiBold mb-3">To</Text>
+
+        <GoogleTextInput
+          icon={icons.map}
+          initialLocation={destinationAddress!}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="transparent"
+          handlePress={(location) => setDestinationLocation(location)}
+        />
+      </View>
+      <CustomButton
+        title="Find Now"
+        onPress={() => router.push("/(root)/confirm-ride")}
+        className="mt-5"
+      />
 
     </RideLayout>
   );
